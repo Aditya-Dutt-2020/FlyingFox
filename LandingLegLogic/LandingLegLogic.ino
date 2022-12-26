fe7967d72eb0c9e39c9f2ccf9ebc11828af5daf7
adimachinelearning <duttaditya@gmail.com>
2022-12-26 15:44:29 -0500

    Working Landing Leg Logic

--- LandingLegLogic/LandingLegLogic.ino
+++ LandingLegLogic/LandingLegLogic.ino
@@ -1,9 +1,36 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
+#define echo 2
+#define trig 3
+#define THRESHDIST 50
 
+
+Servo myservo;
+
+long duration;
+int distance;
+
+
+void setup() {
+  pinMode(echo, INPUT);
+  pinMode(trig, OUTPUT);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  myservo.write(getDist() > THRESHDIST ? 0 : 180);
+}
 
+
+int getDist()
+{
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+
+  duration = pulseIn(echo, HIGH);
+  distance = duration * 0.034 / 2;
+  return distance;
 }
