12160c9af282e5c467b73f8534f66f1254e8d27a
adimachinelearning <duttaditya@gmail.com>
2022-12-26 16:39:39 -0500

    sample image scanning to sim drone movement

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+import cv2
+import numpy as np
+LASTPRESSED = 0
+pos = [0,0]
+def getFrame():
+    global LASTPRESSED, pos
+    orig = cv2.imread("TestingCV Picture.png")
+    frameSize = (300,400)
+    moveSpeed = 30
+    cropped = orig[0:frameSize[0], 0:frameSize[1]]
+    if LASTPRESSED == ord('w'):
+        pos[0] = pos[0]-moveSpeed if pos[0]-moveSpeed >= 0 else pos[0]
+    elif LASTPRESSED == ord('s'):
+        pos[0] = pos[0] + moveSpeed if pos[0] + moveSpeed <= orig.shape[0]-frameSize[0] else pos[0]
+    elif LASTPRESSED == ord('a'):
+        pos[1] = pos[1] - moveSpeed if pos[1] - moveSpeed >= 0 else pos[1]
+    elif LASTPRESSED == ord('d'):
+        pos[1] = pos[1] + moveSpeed if pos[1] + moveSpeed <= orig.shape[1] - frameSize[1] else pos[1]
+    cropped = orig[pos[0]:pos[0]+frameSize[0], pos[1]:pos[1]+frameSize[1]]
+    return cropped
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+while True:
+    LASTPRESSED = cv2.waitKey(0)
+    frame = getFrame()
+    cv2.imshow('testing', frame)
+    if LASTPRESSED == ord('q'):
+        cv2.destroyAllWindows()
+        break
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
